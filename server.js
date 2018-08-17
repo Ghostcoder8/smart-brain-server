212868d41c3f199764ff93c97dd7075fb4da01ed
Ben Collins <ben@Bens-MacBook-Air.local>
2018-08-17 18:37:10 -0500

    fixing db connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-crystalline-81194',
-    user: 'ben',
-    password: '',
-    database: 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
