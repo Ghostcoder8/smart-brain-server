0a798d5d086bd8b2b2a431d616d6254d054d1be1
Ben Collins <ben@Bens-MacBook-Air.local>
2018-08-17 19:35:31 -0500

    configure variable

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-  apiKey: '1253482db84942cd9b212ed1cb196170'
+  apiKey: process.env.API_CLARIFAI
 });
 
 const handleApiCall = (req, res) => {
